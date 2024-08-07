62f1bd60bb5f77a69e0602fc993f83fc20be67e7
eliteReaper <parthapaul47@gmail.com>
2024-08-07 22:41:25 +0530

    Update internet analysis messages and make it more verbose

--- Internet_Analyzer/internet_analyzer.sh
+++ Internet_Analyzer/internet_analyzer.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 while true; do
+    echo "Finding connected devices..."
     arp -a > connected_devices
+    echo "Devices found:"
+    cat connected_devices
+    echo "Performing speedtest..."
     speedtest -f csv --output-header -u MB/s > current_speed.csv
+    echo "Analyzing speedtest results..."
+    rm ./log.txt
     python3 ./internet_analyzer.py
+    echo "Clearing artifacts..."
     rm ./connected_devices
     rm ./current_speed.csv
-    sleep 300
+    echo "Done"
+    sleep 10 # seconds
 done
